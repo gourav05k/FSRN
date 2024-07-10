150c4b809ed9127d220eb6443caa214026bf3be2
Gourav <kamboj05g@gmail.com>
2024-07-11 00:44:33 +0530

    modified style for cart

--- Delicious/src/components/Cart.jsx
+++ Delicious/src/components/Cart.jsx
@@ -52,45 +52,41 @@ function Cart() {
             </div>
           </section>
           :
-          <div className="container mx-auto mt-10">
-            <div className="flex shadow-md my-10">
-              <div className="w-3/4 bg-white px-10 py-10">
-                <div className="flex justify-between border-b pb-8">
-                  <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-                  <h2 className="font-semibold text-2xl">{cartItems.length} Items</h2>
+          <div className="container mx-auto mt-4">
+
+            <div className="md:flex justify-center min-w-full md:min-w-fit mx-auto">
+
+              <div className="w-full md:w-3/4 bg-white px-10 py-10 min-w-min md:ml-10 md:mr-0 md:min-w-min text-nowrap">
+                <div className="flex justify-between border-b pb-8 mx-auto min-w-max md:min-w-fit gap-72 md:gap-60 text-nowrap">
+                  <h1 className="font-semibold text-2xl min-w-max">Shopping Cart</h1>
+                  <h2 className="font-semibold text-2xl min-w-max">{cartItems.length} Items</h2>
                 </div>
-                <div className="flex mt-10 mb-5 mr-0">
+                <div className="flex mt-10 mb-5 mx-0 min-w-max text-nowrap ">
                   <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/6 ">Product Details</h3>
-                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Quantity</h3>
-                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Price</h3>
-                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Total</h3>
-                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center">Remove item</h3>
+                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center ">Quantity</h3>
+                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center ">Price</h3>
+                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center ">Total</h3>
+                  <h3 className="font-semibold text-gray-600 text-xs uppercase w-1/6 text-center ">Remove item</h3>
                   {/* <p>Remove</p> */}
                 </div>
                 {cartItems.map((item) => (
                   <div key={item._id} className="flex items-center hover:bg-gray-100 -mx-6 px-6 py-5">
-                    <div className="flex w-2/6">
-                      <div className="w-20">
-                        <img className="h-full" src={`${menuImageBaseUrl}${item.imageId}`} alt="Item image" />
-                      </div>
-                      <div className="flex flex-col justify-between ml-4 flex-grow">
-                        <span className="font-bold text-sm">{item.name}</span>
-                        {/* <br /> */}
-                        {/* <span className="text-red-500 text-xs">{item.details}</span> */}
-                      </div>
+                    <div className="flex w-2/6 justify-start items-center truncate md:min-w-min">
+                      <img className="w-1/3 object-cover object-center rounded-xl" src={`${menuImageBaseUrl}${item.imageId}`} alt="Item image" />
+                      <h3 className="w-2/3 md:text-wrap flex items-center text-start ml-2 font-bold text-xs min-w-min sm:min-w-min overflow-hidden sm:line-clamp-2 lg:line-clamp-3 ">{item.name}</h3>
                     </div>
-                    <div className="flex justify-center w-1/6">
+                    <div className="flex justify-center w-1/6 min-w-max">
                       <button onClick={() => handleRemoveItem(item)}>
                         <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M32 256C32 238.3 46.3 224 64 224H384C401.7 224 416 238.3 416 256C416 273.7 401.7 288 384 288H64C46.3 288 32 273.7 32 256z" /></svg>
                       </button>
-                      <span className="mx-2 border text-center w-8">{item.quantity}</span>
+                      <span className="mx-2 border text-center w-8 min-w-max">{item.quantity}</span>
                       <button onClick={() => handleAddItem(item)}>
                         <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M432 256C432 273.7 417.7 288 400 288H288V400C288 417.7 273.7 432 256 432C238.3 432 224 417.7 224 400V288H112C94.3 288 80 273.7 80 256C80 238.3 94.3 224 112 224H224V112C224 94.3 238.3 80 256 80C273.7 80 288 94.3 288 112V224H400C417.7 224 432 238.3 432 256z" /></svg>
                       </button>
                     </div>
-                    <span className="text-center w-1/6 font-semibold text-sm">&#8377; {item.price}</span>
-                    <span className="text-center w-1/6 font-semibold text-sm">&#8377; {(item.price * item.quantity).toFixed(2)}</span>
-                    <button onClick={() => handleEmptyItem(item)} className="flex w-1/6 justify-center font-semibold hover:text-red-600 text-gray-500 text-xs">
+                    <span className="text-center w-1/6 font-semibold text-sm min-w-max">&#8377; {item.price}</span>
+                    <span className="text-center w-1/6 font-semibold text-sm min-w-max">&#8377; {(item.price * item.quantity).toFixed(2)}</span>
+                    <button onClick={() => handleEmptyItem(item)} className="flex w-1/6 justify-center font-semibold hover:text-red-600 text-gray-500 text-xs min-w-max">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                       </svg>
@@ -103,27 +99,29 @@ function Cart() {
                 </Link>
               </div>
 
-              <div id="summary" className="w-1/4 px-8 py-10">
-                <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+              <div id="summary" className="md:w-1/4 px-8 py-10 sm:mx-0 md:-ml-10 lg:mx-0 min-w-max md:min-w-fit lg:min-w-max max-w-max ">
+                <h1 className="font-semibold text-2xl border-b pb-8 ">Order Summary</h1>
                 <div className="flex justify-between mt-10 mb-5">
-                  <span className="font-semibold text-sm uppercase">Subtotal</span>
-                  <span className="font-semibold text-sm">&#8377; {calculateSubtotal().toFixed(2)}</span>
+                  <span className="font-semibold text-sm uppercase w-2/3">Subtotal</span>
+                  <span className="flex justify-end font-semibold text-sm w-1/3">&#8377; {calculateSubtotal().toFixed(2)}</span>
                 </div>
                 <div className="flex justify-between mt-10 mb-5">
-                  <span className="font-semibold text-sm uppercase">Delivery Fee</span>
-                  <span className="font-semibold text-sm">&#8377; {shippingEstimate.toFixed(2)}</span>
+                  <span className="font-semibold text-sm uppercase w-2/3">Delivery Fee</span>
+                  <span className="flex justify-end font-semibold text-sm w-1/3">&#8377; {shippingEstimate.toFixed(2)}</span>
                 </div>
                 <div className="flex justify-between mt-10 mb-5">
-                  <span className="font-semibold text-sm uppercase">GST + Restaurant Charges</span>
-                  <span className="font-semibold text-sm">&#8377; {taxEstimate}</span>
+                  <span className="font-semibold text-sm uppercase text-wrap w-2/3">GST + Restaurant Charges</span>
+                  <span className="flex justify-end font-semibold text-sm w-1/3">&#8377; {taxEstimate}</span>
                 </div>
                 <div className="flex justify-between mt-10 mb-5">
-                  <span className="font-semibold text-sm uppercase">Order Total</span>
-                  <span className="font-semibold text-sm">&#8377; {orderTotal}</span>
+                  <span className="font-semibold text-sm uppercase w-2/3">Order Total</span>
+                  <span className="flex justify-end font-semibold text-sm w-1/3">&#8377; {orderTotal}</span>
                 </div>
                 <button className="bg-orange-500 font-semibold hover:bg-orange-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
               </div>
+
             </div>
+
           </div>
       }
     </>
